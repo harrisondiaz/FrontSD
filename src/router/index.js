087880b9cc3dd309f0a8bbd2cd1f1b4515bf729e
jhoia <harrison.diaz@uptc.edu.co>
2022-11-08 17:43:34 -0500

    Img cambiadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/login.vue'
-const name_user = ''
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
