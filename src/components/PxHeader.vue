92e3707838371ccaec6bc1e8254b895d8424ebcb
jhoia <harrison.diaz@uptc.edu.co>
2022-11-22 18:32:24 -0500

    CAmbios

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg bg-dark">
     <div class="container-fluid">
       <router-link to="/assigature"><img src="https://cdn-icons-png.flaticon.com/512/2645/2645163.png" alt="Logo pc"></router-link>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link dropdown-toggle text-light"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -12,9 +12,7 @@
               <li><router-link to="/assignature/view"><a class="dropdown-item">Ver Listado</a></router-link></li>
               <li><hr class="dropdown-divider"></li>
               <li><router-link to="/assignature/register"><a class="dropdown-item" >Registrar</a></router-link></li>
-
             </ul>
-          
           </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle text-light"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
