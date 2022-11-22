7b6c2056ca79ba20e7ec12907bc59aaab894dba2
jhoia <harrison.diaz@uptc.edu.co>
2022-11-22 18:42:09 -0500

    Bugs uwu

--- src/components/listedStudient.vue
+++ src/components/listedStudient.vue
@@ -46,7 +46,7 @@
 
 <!--      <td>{{n.estado_materia}}</td>-->
 
-      <td><img src="https://comunidadvalorant.es/wp-content/uploads/2021/04/guia-killjoy-valorant.jpg" style="width: auto"></td>
+      <td><img src="https://comunidadvalorant.es/wp-content/uploads/2021/04/guia-killjoy-valorant.jpg" style="width: 10px;height: 10%"></td>
       <td class=""  role="button" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
 </svg>
