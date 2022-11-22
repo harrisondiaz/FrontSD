9a1927135af6fa10a9fa1fe317085bf4fd5b21f7
jhoia <harrison.diaz@uptc.edu.co>
2022-11-22 18:36:36 -0500

    Ultimos Cambios

--- src/components/listed.vue
+++ src/components/listed.vue
@@ -43,7 +43,6 @@
 <!--      <td>{{n.estado_materia}}</td>-->
       <td v-if="n.estado_materia==1">Activo</td>
       <td v-else>Deshabilitado</td>
-
       <td class=""  role="button" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
       </svg>
