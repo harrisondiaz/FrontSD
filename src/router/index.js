593e214dabc3573fd6b9dda731c921a2f4cd70d2
jhoia <harrison.diaz@uptc.edu.co>
2022-11-26 20:26:50 -0500

    Bugs uwu

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/*',
+      component: 404
+    },
+    {
       path: '/',
       component: Home
     },
