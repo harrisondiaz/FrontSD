cb80540d767215c47a09c1e53992c253621df6ca
jhoia <harrison.diaz@uptc.edu.co>
2022-11-26 21:58:35 -0500

    Bugs :3

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -2,6 +2,9 @@
   <nav class="navbar navbar-expand-lg bg-dark">
     <div class="container-fluid">
       <router-link to="/assigature"><img src="https://cdn-icons-png.flaticon.com/512/2645/2645163.png" alt="Logo pc"></router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
@@ -14,16 +17,16 @@
               <li><router-link to="/assignature/register"><a class="dropdown-item" >Registrar</a></router-link></li>
             </ul>
           </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle text-light"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Inscripciones
-              </a>
-              <ul class="dropdown-menu dropdown-menu-dark">
-                <li><router-link to="/inscription"><a class="dropdown-item">Ver Listado</a></router-link></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><router-link to="/inscription/register"><a class="dropdown-item" >Registrar</a></router-link></li>
-                </ul>
-            </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-light"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Inscripciones
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><router-link to="/inscription"><a class="dropdown-item">Ver Listado</a></router-link></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><router-link to="/inscription/register"><a class="dropdown-item" >Registrar</a></router-link></li>
+            </ul>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-light"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Estudiantes
@@ -32,7 +35,7 @@
               <li><router-link to="/studient"><a class="dropdown-item" >Ver Listado</a></router-link></li>
               <li><hr class="dropdown-divider"></li>
               <li><router-link to="/studient/register"><a class="dropdown-item" >Registrar</a></router-link></li>
-              </ul>
+            </ul>
           </li>
         </ul>
         <router-link to="/"><button class="btn btn-outline-light">Cerrar Session</button></router-link>
